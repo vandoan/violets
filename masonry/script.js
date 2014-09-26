c7b5eeb17db7468819bc948f5305da196784ad7b
Van Doan <vdoan09@gmail.com>
2014-09-26 15:24:29 -0400

    completed the first masonry project

---
+++ masonry/script.js
@@ -0,0 +1,33 @@
+
+/**
+ * Base js functions
+ */
+
+$(document).ready(function(){
+    var $container = $('.container');
+
+    var gutter = 30;
+    var min_width = 300;
+    $container.imagesLoaded( function(){
+        $container.masonry({
+            itemSelector : '.box',
+            gutterWidth: gutter,
+            isAnimated: true,
+              columnWidth: function( containerWidth ) {
+                var box_width = (((containerWidth - 2*gutter)/3) | 0) ;
+
+                if (box_width < min_width) {
+                    box_width = (((containerWidth - gutter)/2) | 0);
+                }
+
+                if (box_width < min_width) {
+                    box_width = containerWidth;
+                }
+
+                $('.box').width(box_width);
+
+                return box_width;
+              }
+        });
+    });
+});
---
+++ note.md
@@ -0,0 +1,4 @@
+- masonry1
+	- http://www.maurizioconventi.com/2012/06/05/a-fluid-layout-with-jquery-masonry/
+
+	
